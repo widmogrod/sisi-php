3f7db24a86914143cd0a95821dcd223497d1234d
widmogrod <widmogrod@c6cc6730-2031-3d53-7f3a-7a209490133d>
2010-07-07 07:47:07 +0000

    deploy

---
+++ library/functions.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @param string $name
+ */
+function partial($name)
+{
+	$name = basename($name);
+	$partial = SKIN_PATHNAME . '/' . SKIN_NAME . '/_partial/' . $name . '.php';
+	
+	if (!is_file($partial))
+		return spritf('<!-- partial(%s): nie istnieje-->', $name);
+
+	ob_start();
+        include $partial;
+
+    return ob_get_clean();
+}
+
+/**
+ * @param string $name
+ */
+function resource_path($name)
+{
+	$path = RESOURCES_PATH . '/' . $name;
+	return $path;
+}
+
+/**
+ * @param string $name
+ */
+function css_resource($name)
+{
+	$path = resource_path($name);
+	$path = sprintf('<link href="%s" type="text/css" rel="stylesheet" />', $path);
+	return $path;
+}
+
+/**
+ * @param string $name
+ */
+function js_resource($name)
+{
+	$path = resource_path($name);
+	$path = sprintf('<script type="text/javascript" src="%s"></script>', $path);
+	return $path;
+}
+
+
+
+/**
+ * @param string $name
+ */
+function skin_resource_path($name)
+{
+	$path = SKIN_PATH . '/' . SKIN_NAME . '/' . $name;
+	return $path;
+}
+
+/**
+ * @param string $name
+ */
+function css_skin($name)
+{
+	$path = skin_resource_path($name);
+	$path = sprintf('<link href="%s" type="text/css" rel="stylesheet" />', $path);
+	return $path;
+}
+
+/**
+ * @param string $name
+ */
+function js_skin($name)
+{
+	$path = skin_resource_path($name);
+	$path = sprintf('<script type="text/javascript" src="%s"></script>', $path);
+	return $path;
+}
+
+
---
+++ skins/krakplast/_partial/html_head.php
@@ -0,0 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="google-site-verification" content="MxQP3nYIIooyj3qQG7SSv4Uzq-l7In8vu7NPk4_60fE" />
+
+<title>KRAK PLAST - Najwyższej jakości wyroby ze stali, kamienia oraz wysokiej klasy automatyka do bram!</title>
+<meta name="keywords" content="KRAK PLAST - Najwyższej jakości wyroby ze stali, kamienia oraz wysokiej klasy automatyka do bram!" />
+<meta name="description" content="KRAK PLAST - Najwyższej jakości wyroby ze stali, kamienia oraz wysokiej klasy automatyka do bram!" />
+
+<link href='http://fonts.googleapis.com/css?family=Josefin+Sans+Std+Light' rel='stylesheet' type='text/css'>
+<link href=' http://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+<link href=' http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:regular,bold' rel='stylesheet' type='text/css'>
+<link href=' http://fonts.googleapis.com/css?family=OFL+Sorts+Mill+Goudy+TT:italic' rel='stylesheet' type='text/css'>
+
+<?php 
+	print css_skin('css/global.css');
+	
+	print js_skin('js/jquery-1.4.2.min.js');
+	print js_skin('js/jquery.jcarousel.min.js');
+	print css_skin('js/jcarousel/krakplast/skin.css');
+?>
