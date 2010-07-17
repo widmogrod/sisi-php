087930ca5c6f0fc14e28032af0bd7562dfa9dbd5
widmogrod <widmogrod@c6cc6730-2031-3d53-7f3a-7a209490133d>
2010-07-17 07:29:40 +0000

    Aktualizacja strony kontaktu

--- trunk/pages/kontakt.php
+++ trunk/pages/kontakt.php
@@ -5,13 +5,22 @@
 			<a class="smore more red" href="index.php?action=products" >&laquo; Zobacz nasze realizacje</a>
 		</div>
 		
-		<p class="display">Zapraszamy do kontaktu telefonicznego: <br/>+48 123 123 123</p>
+		<p class="display">Zaufanie jakim obdarza nas coraz szersze grono klientów jest naszym największym sukcesem!</p>
 	</div>
 </div>
 
 <div id="content" class="clearfix">
 	<div class="grid_16 clearfix">
-	<p style="text-align:center">Zaufanie jakim obdarza nas coraz szersze grono klientów jest naszym największym sukcesem!</p>
+
+		<h2>KRAKPLAST Jacek Zawada</h2>
+		<p>Wysoki Małe 8<br/>
+			28-210 Bogoria<br/><br/>
+	
+			Tel: 693 180 530<br/>
+			E-mail: <a href="mailto:biuro@krak-plast.pl">biuro@krak-plast.pl</a><br/></p>
+
+		<p>NIP: 866-158-28-94<br/>
+			REGON: 260383784<br/>
+			Nr konta: 80 1240 3002 1111 0010 0426 0906</p>
 	</div>
 </div>
-
