c368de0393d46a83aa03a01877213aaf6c48cdc0
widmogrod <widmogrod@c6cc6730-2031-3d53-7f3a-7a209490133d>
2010-07-28 10:07:30 +0000

    dodanie licznika odwiedzin

--- trunk/skins/krakplast/_partial/body_foot.php
+++ trunk/skins/krakplast/_partial/body_foot.php
@@ -1,3 +1,36 @@
+<?php
+function licznikOdwiedzin() 
+{
+    $cookieKey               = 'licznik';
+    $cookieIsCounted = (isset($_COOKIE[$cookieKey]) && $_COOKIE[$cookieKey] == 1);
+    $cookieFilename  = dirname(__FILE__) . '/licznik.txt';
+
+    // ile zliczono
+    $visits = is_readable($cookieFilename)
+            ? (!($visits = file_get_contents($cookieFilename)) ? -1 : $visits)
+            : 1;
+
+    // czy zliczony juz wczesniej
+    if (!$cookieIsCounted) {
+            // ustaw ze liczony na 24h
+            setcookie($cookieKey, 1, time() + 60*60*24);
+            // powiekszamy wizyty
+            $visits += 1;
+    }
+
+    // zapis
+    if (!file_put_contents($cookieFilename, $visits)) {
+            print 'nie można zapisać licznika';
+    } else {
+            @chmod($cookieFilename, 0666);
+    }
+
+    // ile zliczono
+    return $visits;
+}
+
+?>
 <div id="foot" class="grid_16 clearfix small">
 	<p>&copy; 2010 Krak-Plast. Projekt i wykonanie <a href="http://widmogrod.info" title="Tworzenie stron internetowych">widmogrod.info</a>.</p>
+	<p>Odwiedzin: <?php print licznikOdwiedzin() ?></p>
 </div>
