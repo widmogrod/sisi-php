8660397a2085f0837b501c084b5647f5e46e514c
widmogrod <widmogrod@c6cc6730-2031-3d53-7f3a-7a209490133d>
2010-07-28 06:35:55 +0000

    formularzkontaktowy

--- trunk/pages/zapytanie-ofertowe.php
+++ trunk/pages/zapytanie-ofertowe.php
@@ -12,7 +12,47 @@
 <div id="content" class="clearfix">
 	<div class="grid_16 clearfix">
 		<div class="grid_5 clearfix">
-			Wkrótce...
+<?php
+
+require_once 'forms/Contact.php';
+$form = new Form_Contact();
+
+require_once 'Zend/View.php';
+$view = new Zend_View();
+$form->setView($view);
+
+if (!empty($_POST)) 
+{
+	if ($form->isValid($_POST))
+	{
+		$mail = '<dl>';
+		foreach($form->getElements() as $element)
+		{
+			$label = $element->getLabel();
+			$value = $element->getValue();
+
+			$mail .= sprintf('<dt><b>%s</b></dt><dl>%s</dl>', $label, $value);
+		}
+		$mail .= '</dl>';
+
+		require_once 'Zend/Mail.php';
+		$mail = new Zend_Mail();
+		$mail->setFrom('biuro@krak-plast.pl', 'Jacek Zawada');
+		$mail->addTo('biuro@krak-plast.pl');
+		$mail->setSubject('Formularz zapytaniowy');
+		$mail->setBodyHtml($mail,'UTF-8');
+
+		try {
+			$mail->send();
+		} catch (Zend_Mail_Exception $e) {
+			print '<p>'.$e->getMessage().'</p>';
+		}
+	}
+}
+
+print $form->render();
+
+?>
 		</div>
 	</div>
 </div>
