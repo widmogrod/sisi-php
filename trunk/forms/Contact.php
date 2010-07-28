d06cdadf9f2f4f91d66c01b1f4c0215317660f25
widmogrod <widmogrod@c6cc6730-2031-3d53-7f3a-7a209490133d>
2010-07-28 09:52:17 +0000

    aktualizacja formularza zapytaniowego

--- trunk/forms/Contact.php
+++ trunk/forms/Contact.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Zend/Form.php';
+require_once 'Zend/Validate/StringLength.php';
 
 class Form_Contact extends Zend_Form
 {
@@ -8,35 +9,34 @@ class Form_Contact extends Zend_Form
 
 		$this
 			->addElement('text','username', array(
-				'label' => 'Imię i nazwisko',
-				'options' => array(
-					'required' => true
+				'label' => '↓ Imię i nazwisko',
+				'required' => true,
+				'validators' => array(
+					'strlen' => new Zend_Validate_StringLength(array('min' => 5))
 				)
 			))
 			->addElement('text','email', array(
-				'label' => 'Adres Email',
-				'options' => array(
-					'required' => true,
-					'validators' => array('EmailAddress')
-				)
+				'label' => '↓ Adres Email',
+				'required' => true,
+				'validators' => array('EmailAddress')
 			))
 			->addElement('text','phone', array(
-				'label' => 'Numer telefonu',
-				'options' => array(
-					'required' => false
+				'label' => '↓ Numer telefonu',
+				'required' => false,
+				'validators' => array(
+					'strlen' => new Zend_Validate_StringLength(array('min' => 7))
 				)
 			))
-			->addElement('text','content', array(
-				'label' => 'Treść zapytania',
-				'options' => array(
-					'required' => true
+			->addElement('textarea','mail-content', array(
+				'label' => '↓ Treść zapytania',
+				'required' => true,
+				'validators' => array(
+					'strlen' => new Zend_Validate_StringLength(array('min' => 10))
 				)
 			))
 			->addElement('submit','submit', array(
 				'label' => 'Wyślij zapytanie',
-				'options' => array(
-					'ignore' => true
-				)
+				'ignore' => true
 			));
 	}
 }
